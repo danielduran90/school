8b666c32e804bf884e7891cb90f894457f083c2f
Billy Gil <billyarturo06@gmail.com>
2024-09-09 19:53:47 -0500

    Este sigue siendo el primer cambio

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -22,7 +22,7 @@ class EventServiceProvider extends ServiceProvider
 
     /**
      * Register any events for your application.
-     *
+     ** Hola Billy 
      * @return void
      */
     public function boot()
